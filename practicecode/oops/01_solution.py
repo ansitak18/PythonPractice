6dce3eb05e05978308abc9b14e1e38175d003a4d
ansitak18 <ansitabarnwal@gmail.com>
2026-09-11 01:25:25 +0530

    Inheritance question solved

--- practicecode/oops/01_solution.py
+++ practicecode/oops/01_solution.py
@@ -2,14 +2,28 @@ class Car:
     def __init__(self, brand, model): #self works as this works in js
         self.brand = brand
         self.model = model
-
+#2nd question 
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+    
+#inheritance 3rd question
+class ElectricCar(Car): 
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand,model) #inheriting elements from the above class Car using super keyword 
+        self.battery_size = battery_size
+# 3rd question 
+my_tesla = ElectricCar("Tesla", "Model S", "85kWH")
+print(my_tesla.model)
+print(my_tesla.full_name())#it can also access full_name which is in the Car class but inside different function 
 
 my_car = Car("Toyota", "Corolla") #object created
 print(my_car.brand)
 print(my_car.model)
+print(my_car.full_name()) #2nd question
 
 my_new_car = Car("Tata", "Safari")
 print(my_new_car.model)
 
 #__init__() is a constructor
 # and if we dont write self keyword then classes and objects wont be able to access each other
+#so self acts like a connection between class and objects
--- practicecode/oops/questions.md
+++ practicecode/oops/questions.md
@@ -5,7 +5,10 @@
 ## Problem : Create a Car class with attributes like brand and model. Then create an instance of this class.
 
 >2 Class Method and Self 
+## Problem : Add a method to the Car class that displays the full name of the car (brand and model).
+
 >3 Inheritance
+## Problem : Create an ElectricCar class that inherits from the Car class and has an additional attribute battery_size.
 >4 Encapsulation
 >5 Class Variables
 >6 Static Method
