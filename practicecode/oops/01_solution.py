6661bc9c740e548b0428bb80ffed24649d4ab421
ansitak18 <ansitabarnwal@gmail.com>
2026-09-13 00:47:08 +0530

    Polymorphism and Class Variables

--- practicecode/oops/01_solution.py
+++ practicecode/oops/01_solution.py
@@ -1,31 +1,51 @@
 class Car:
+
+    #total cars created - classes and variables
+    total_cars = 0
+
+
     def __init__(self, brand, model): #self works as this works in js
         self.__brand = brand
         self.model = model
+        Car.total_cars += 1 # or self.total_car
 
-        # 4th question about encapsualtion making private variable by adding two underscore __attributename
+
+
+    # 4th question about encapsualtion making private variable by adding two underscore __attributename
     def get_brand(self):
         return self.__brand + "!" # can add more characters by simply using + 
     
 #2nd question 
     def full_name(self):
         return f"{self.__brand} {self.model}"
+
+#for polymorphism
+    def fuel_type(self):
+        return 'Petrol or Diesel'
     
 #inheritance 3rd question
 class ElectricCar(Car): 
     def __init__(self, brand, model, battery_size):
         super().__init__(brand,model) #inheriting elements from the above class Car using super keyword 
         self.battery_size = battery_size
+
+    def fuel_type(self):
+        return 'Electric charge'
 # 3rd question 
 my_tesla = ElectricCar("Tesla", "Model S", "85kWH")
 print(my_tesla.model)
 print(my_tesla.full_name())#it can also access full_name which is in the Car class but inside different function 
+print(my_tesla.fuel_type()) #polymorphism
 
-my_car = Car("Toyota", "Corolla") #object created
+Car("Tata", "Safari") #object creation
+#print(safari.total_cars) # dont access the numbers like this as it may give the wrong result 
+print(Car.total_cars) #correct way 
+
+my_car = Car("Toyota", "Corolla") #object created without holding any reference for the object created
 #print(my_car.brand) #does not give direct access because brand attribute is now private
 print(my_car.model)
 print(my_car.full_name()) #2nd question
-
+print(my_car.fuel_type()) #polymorphism
 my_new_car = Car("Tata", "Safari")
 print(my_new_car.model)
 
--- practicecode/oops/questions.md
+++ practicecode/oops/questions.md
@@ -2,19 +2,24 @@
 
 
 >1 Basics Class and Object 
-## Problem : Create a Car class with attributes like brand and model. Then create an instance of this class.
+### Problem : Create a Car class with attributes like brand and model. Then create an instance of this class.
 
 >2 Class Method and Self 
-## Problem : Add a method to the Car class that displays the full name of the car (brand and model).
+### Problem : Add a method to the Car class that displays the full name of the car (brand and model).
 
 >3 Inheritance
-## Problem : Create an ElectricCar class that inherits from the Car class and has an additional attribute battery_size.
+### Problem : Create an ElectricCar class that inherits from the Car class and has an additional attribute battery_size.
 
 >4 Encapsulation
-## Problem : Modify the Car class to encapsulate the brand attribute, making it private, and provide a getter method for it.
+### Problem : Modify the Car class to encapsulate the brand attribute, making it private, and provide a getter method for it.
 
->5 Class Variables
->6 Static Method
->7 Property Decorators
->8 Class Inheritance and isinstance() function
->9 Multiple Inheritance
+>5 Polymorphism 
+### Problem : Demonstrate polymorphism by defining a method fuel_type in both Car and ElectricCar classes, but with different behaviours.
+
+>6 Class Variables
+### Problem : Add a class variable to Car class that keeps track of the number of cars created.
+
+>7 Static Method
+>8 Property Decorators
+>9 Class Inheritance and isinstance() function
+>10 Multiple Inheritance
